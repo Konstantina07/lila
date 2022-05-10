a4b720e1d1c97323b15be1ac4912a61fd892fd8a
Konstantinos07 <gkizisk@gmail.com>
2022-05-10 11:28:53 +0300

    update dict
    *add "monkey", "autist", "autistic"
    *fix bad regex for dumbass
    * update dict comments

--- modules/shutup/src/main/Dictionary.scala
+++ modules/shutup/src/main/Dictionary.scala
@@ -2,7 +2,7 @@ package lila.shutup
 
 /**   - words are automatically leetified. "tit" will also match "t1t", "t-i-t", and more.
   *   - words do not partial match. "anal" will NOT match "analysis".
-  *   - en, es and de words are automatically pluralized. "tit" will also match "tits", "cabron" will also
+  *   - en, es, de and fr words are automatically pluralized. "tit" will also match "tits", "cabron" will also
   *     match "cabrones" etc.
   *   - For en only: Past tense of last word in a string matches: "cheat" will also match "cheated", "you
   *     suck" will also match "you sucked" but "kill you" will NOT match "killed you"
@@ -20,7 +20,9 @@ afraid
 anal(plug|sex|)
 anus
 arse(hole|wipe|)
+ass
 ass?(hole|fag)
+autist(ic|)
 aus?c?hwitz
 bastard?
 be[ea]+ch
@@ -49,7 +51,7 @@ dic?k(head|face|suc?ker|)
 dildo
 dogg?ystyle
 douche(bag|)
-(dumb)?ass?
+dumb(ass?|)
 dyke
 engine
 fck(er|r|u|k|t|ing?|ign|tard?|face|off?|)
@@ -77,6 +79,7 @@ lo+ser
 masturbat(e|ion|ing)
 milf
 molest(er|)
+monkey
 moron
 mother(fuc?k(er|)|)
 mthrfckr
